d3eecf907f5650360387f7e9cd20494a720aba6c
Alex Feldman <alexfeldman93@gmail.com>
2018-08-24 13:12:47 -0700

    clock with speech

--- clock.py
+++ clock.py
@@ -1,11 +1,14 @@
 import inflect
+import subprocess
 
 
 def conversion(time):
     p = inflect.engine()
     time = list(map(int, time.split(':')))
+    if not (0 <= time[0] <= 24 and 0 <= time[1] < 60):
+        return 'There is no time like the present.'
     hour = time[0] % 12 or 12
-    period = 'am' if time[0] >= 12 else 'pm'
+    period = 'am' if time[0] <= 12 else 'pm'
     if not time[1]:
         minute = ''
     elif time[1] >= 10:
@@ -15,10 +18,11 @@ def conversion(time):
     return 'Its {} {} {}'.format(p.number_to_words(hour), minute, period)
 
 
+def say(time_txt):
+    subprocess.call('say {}'.format(time_txt), shell=True)
+
 
 if __name__ == '__main__':
     time = input('Input: ')
     time_txt = conversion(time)
-    print(time_txt)
-    # speak(time_txt)
-    # main()
+    say(time_txt)
